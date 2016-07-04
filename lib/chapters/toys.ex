3d19eb5c9bca2d590e6f4a60b4ecba8b07327c81
Greg Cline <gregrcline@gmail.com>
2016-07-04 12:59:53 -0400

    Toys chapter done

---
+++ lib/chapters/toys.ex
@@ -0,0 +1,19 @@
+defmodule Toys do
+  @spec car(list) :: any
+  def car(list), do: hd(list)
+
+  @spec cdr(list) :: list
+  def cdr(list), do: tl(list)
+
+  @spec cons(any, list) :: list
+  def cons(elem, list) do
+    [elem | list]
+  end
+
+  @spec null?(list) :: boolean
+  def null?([]), do: true
+  def null?(x) when is_list(x), do: false
+
+  @spec eq?(atom, atom) :: boolean
+  def eq?(x, y) when is_atom(x) and is_atom(y), do: x == y
+end
---
+++ test/toys_test.exs
@@ -0,0 +1,62 @@
+Code.load_file("toys.ex")
+
+ExUnit.start
+ExUnit.configure exclude: :pending, trace: true
+
+defmodule ToysTest do
+  use ExUnit.Case, async: true
+
+  describe "Toys.car/1" do
+    test "returns the first element in a list" do
+      assert Toys.car([:a, :b, :c]) == :a
+      assert Toys.car([[:a, :b], [:c, :d]]) == [:a, :b]
+      assert Toys.car(Toys.car([[:a]])) == :a
+    end
+
+    test "errors on an empty list" do
+      catch_error Toys.car([])
+    end
+  end
+
+  describe "Toys.cdr/1" do
+    test "returns the tail of a list" do
+      assert Toys.cdr([:a, :b, :c]) == [:b, :c]
+      assert Toys.cdr([[:a, :b], [:c, :d]]) == [[:c, :d]]
+    end
+
+    test "errors on an empty list" do
+      catch_error Toys.cd([])
+    end
+  end
+
+  describe "Toys.cons/2" do
+    test "takes something and a list and puts something on the front of the list" do
+      assert Toys.cons(:a, [:b, :c]) == [:a, :b, :c]
+      assert Toys.cons(:a, []) == [:a]
+      assert Toys.cons([:a, :b], [:c]) == [[:a, :b], :c]
+    end
+  end
+
+  describe "Toys.null?/1" do
+    test "returns true if a list is empty and false if it isn't" do
+      assert Toys.null?([])
+      refute Toys.null?([:a])
+      refute Toys.null?([[]])
+    end
+
+    test "errors on non-lists" do
+      catch_error Toys.null?(:a)
+    end
+  end
+
+  describe "Toys.eq?/2" do
+    test "returns true if two atoms are equal and false otherwise" do
+      assert Toys.eq?(:a, :a)
+      refute Toys.eq?(:a, :b)
+    end
+
+    test "errors on non-atoms" do
+      catch_error Toys.eq?([])
+    end
+  end
+end
